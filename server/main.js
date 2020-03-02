55d62c2b23a0dcf0e69edeba0a16891dfad33961
Nader Ardebili <nader@NAs-mbp.broadband>
2020-03-02 13:52:57 +0000

    rudimentary message retention

--- server/main.js
+++ server/main.js
@@ -1,38 +1,39 @@
-var WSS = require('ws').Server;
+var WSS = require("ws").Server;
 
 // Start the server
 var wss = new WSS({ port: 8080 });
 
+const messages = [];
+
 // When a connection is established
-wss.on('connection', function(socket) {
-  console.log('Opened connection ');
+wss.on("connection", function(socket) {
+  console.log("Opened connection ");
 
   // Send data back to the client
-  var json = JSON.stringify({ message: 'Gotcha' });
+  var json = JSON.stringify({ message: "Gotcha" });
   socket.send(json);
 
   // When data is received
-  socket.on('message', function(message) {
-    console.log('Received: ' + message);
+  socket.on("message", function(message) {
+    console.log("Received: " + message);
+    messages.push(message);
+    broadcast();
   });
 
   // The connection was closed
-  socket.on('close', function() {
-    console.log('Closed Connection ');
+  socket.on("close", function() {
+    console.log("Closed Connection ");
   });
-
 });
 
-// Every three seconds broadcast "{ message: 'Hello hello!' }" to all connected clients
 var broadcast = function() {
   var json = JSON.stringify({
-    message: 'Hello hello!'
+    messages
   });
 
   // wss.clients is an array of all connected clients
   wss.clients.forEach(function each(client) {
     client.send(json);
-    console.log('Sent: ' + json);
+    console.log("Sent: " + json);
   });
-}
-setInterval(broadcast, 3000);
+};
